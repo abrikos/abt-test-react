d96adc0f41e5172cd68a2897155140015a592d1c
abrikos <5422791+abrikos@users.noreply.github.com>
2022-01-26 11:54:44 +0900

    add user

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.sass';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {useState} from 'react';
+import {loadDb, addUser} from './mock'
+import {Modal, Button, Form} from 'react-bootstrap';
+import moment from 'moment';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [items, setItems] = useState([])
+    const [show, setShow] = useState(false);
+    const [registration, setRegistration] = useState('');
+    const [activity, setActivity] = useState('');
+    const handleClose = () => {
+        if(!(registration && activity)) return alert('Please select both dates')
+        if(!moment(registration).isValid()) return alert('Invalid registration date')
+        if(!moment(activity).isValid()) return alert('Invalid activity date')
+        setItems(addUser({registration, activity}))
+        setShow(false);
+    }
+    const randDate = () =>{
+        const rnd = Math.floor(Math.random() * (100)) + 1;
+        const rnd2 = Math.floor(Math.random() * (100)) + 1;
+        let r1, r2;
+        if(rnd2 < rnd) {
+            r1 = rnd;
+            r2 = rnd2
+        }else{
+            r2 = rnd;
+            r1 = rnd2
+        }
+        console.log(r1, r2, moment().add(-r1, 'days').format('YYYY-MM-DD'))
+        setRegistration(moment().add(-r1, 'days').format('YYYY-MM-DD'))
+        setActivity(moment().add(-r2, 'days').format('YYYY-MM-DD'))
+    }
+    const handleShow = () => {
+        setRegistration('');
+        setActivity('');
+        setShow(true);
+    }
+
+    useState(() => {
+        setItems(loadDb())
+    })
+
+    return (
+        <div className="App">
+            <Button variant="primary" onClick={handleShow}>
+                Add user
+            </Button>
+            <table>
+                <thead>
+                <tr>
+                    <th>UserId</th>
+                    <th>Registration date</th>
+                    <th>Last activity date</th>
+                </tr>
+                </thead>
+                <tbody>
+                {items.map(item => <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.registration}</td>
+                    <td>{item.activity}</td>
+                    <td>{item.lifeDays}</td>
+                    <td>{item.registeredDays}</td>
+                    <td>
+                        <b-button variant="danger" size="sm">Delete</b-button>
+                    </td>
+                </tr>)}
+                </tbody>
+            </table>
+            <Modal show={show} onHide={()=>setShow(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Registration date</Form.Label>
+                            <Form.Control defaultValue={registration} onChange={({target}) => setRegistration(target.value)} type="calendar" placeholder="YYYY-MM-DD"/>
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+                            <Form.Label>Registration date</Form.Label>
+                            <Form.Control defaultValue={activity} onChange={({target}) => setActivity(target.value)} type="calendar" placeholder="YYYY-MM-DD"/>
+                        </Form.Group>
+                        <Button onClick={randDate}>Random</Button>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={()=>setShow(false)}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/mock.js
@@ -0,0 +1,53 @@
+import moment from 'moment';
+
+const getMaxId = (data) => Math.max(...data.filter(i => !!i.id).map(i => i.id));
+const mock = [
+    {id: 1, registration: '2021-10-12', activity: '2021-11-12'},
+    {id: 2, registration: '2021-11-12', activity: '2021-11-22'},
+    {id: 3, registration: '2021-11-13', activity: '2021-11-25'},
+    {id: 4, registration: '2021-11-14', activity: '2021-12-12'},
+    {id: 5, registration: '2021-11-14', activity: '2021-11-15'},
+    {id: 6, registration: '2021-11-18', activity: '2021-12-12'},
+    {id: 7, registration: '2021-11-18', activity: '2021-12-12'},
+    {id: 8, registration: '2021-11-18', activity: '2021-12-12'},
+]
+
+const addUser = user => {
+    const items = loadDb();
+    items.push(user);
+    return saveDb(items)
+}
+
+const saveDb = data => {
+    let maxId = getMaxId(data);
+    console.log('Save data', maxId);
+    for (const item of data) {
+        if (!item.id) {
+            maxId++;
+            item.id = maxId;
+        }
+    }
+    localStorage.setItem('data', JSON.stringify(data));
+    return loadDb();
+}
+const loadDb = () => {
+    console.log('load data');
+    let stored = []
+    try {
+        stored = JSON.parse(localStorage.getItem('data'));
+    } catch (e) {
+        console.warn(e)
+    }
+    if(!(stored && stored.length)){
+        saveDb(mock);
+        stored = mock;
+    }
+    const ret = stored && stored.length ? stored : mock;
+    for (const item of ret) {
+        item.lifeDays = moment(item.activity).diff(moment(item.registration), 'days');
+        item.registeredDays = moment().diff(moment(item.registration), 'days');
+    }
+    return ret;
+}
+export {saveDb, loadDb, addUser}
+
